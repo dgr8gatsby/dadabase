19e45755286ef1d135775e147916cde9829fc68c
Kevin <kevinhill@live.com>
2021-01-13 22:15:53 -0800

    setup basic server.js

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const path = require ('path'); // Use path to help with directory paths
 const bodyParser = require ('body-parser'); // Use body-parser to help parse POST JSON objects
 // const api = require ('./src/server/api'); // Routes for base server API
 
-cosnt app = express();
+const app = express ();
 const PORT = process.env.PORT;
 
-app.use(bodyParser.urlencoded ({extended: true}));
-app.use(express.static(path.join (__dirname, 'public')));
+app.use (bodyParser.urlencoded ({extended: true}));
+app.use (express.static (path.join (__dirname, 'public')));
+app.use ('/api', api);
+
+app.listen (PORT, err => {
+  if (err) throw err;
+  const hostname = 'localhost';
+  console.log (`application listenting on http://${hostname}:${PORT}`);
+});

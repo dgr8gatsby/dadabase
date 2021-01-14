d5608cb7c0948b92bf33d0f07a6a89bf90d3f1eb
Kevin <kevinhill@live.com>
2021-01-13 22:29:27 -0800

    adding rollup support

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import {terser} from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/app/index.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true,
+  },
+  plugins: [
+    resolve (), // tells Rollup how to find date-fns in node_modules
+    commonjs (), // converts date-fns to ES modules
+    production && terser (), // minify, but only in production
+    copy ({
+      targets: [
+        {src: 'src/app/index.html', dest: 'public'},
+        {src: 'src/app/favicon.ico', dest: 'public'},
+        {src: 'src/app/svg/*', dest: 'public/svg'},
+        {src: 'src/app/screens', dest: 'public'},
+      ],
+    }),
+  ],
+};
---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+/**
+ * index.js is the entry file for rollup. import the base components here
+ */
